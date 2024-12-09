0299e9b854a945bcb788742011ec314b7db5445e
mohammedyh <mohammedyousufhamid@gmail.com>
2024-12-09 18:12:30 +0000

    refactor: split index.js into ES modules
    - Organise assets and css files into separate folders
    - Add favicon

---
+++ src/js/timer.js
@@ -0,0 +1,29 @@
+const pomodoroTime = document.querySelector(".pomodoro__time");
+const sound = document.querySelector("audio");
+let countdown;
+
+export function timer(seconds) {
+  clearInterval(countdown);
+  const start = Date.now();
+  const finish = start + seconds * 1000;
+  displayTimeLeft(seconds);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((finish - Date.now()) / 1000);
+    if (secondsLeft <= 0) {
+      clearInterval(countdown);
+      document.title = "Time Up!";
+      sound.currentTime = 5;
+      sound.play();
+    }
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+export function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const secondsRemaining = seconds % 60;
+  const displayTime = `${minutes}:${secondsRemaining < 10 ? "0" : ""}${secondsRemaining}`;
+  document.title = displayTime;
+  pomodoroTime.textContent = displayTime;
+}
