33d7a0a85c8a7a354c553189cb64f2f8da151e60
mohammedyh <mohammedyousufhamid@gmail.com>
2024-12-10 23:19:43 +0000

    refactor: organise app events into own file
    - Simplify logic for saving and loading app settings
    - Remove commented code from index.js

---
+++ src/js/events.js
@@ -0,0 +1,42 @@
+import { handleSettingsFormSubmit } from "./settings.js";
+import { timer } from "./timer.js";
+
+const modes = document.querySelectorAll(".mode");
+const getStartedMessage = document.querySelector(".get-started");
+const settingsModalPopover = document.querySelector("#settings-modal");
+const settingsModalCloseBtn = document.querySelector(".settings-modal__close");
+const settingsModalApplyButton = document.querySelector(".settings-modal__btn");
+const fontButtons = document.querySelectorAll("[data-font]");
+const accentColorButtons = document.querySelectorAll("[data-accent-color]");
+
+export function setupEventListeners() {
+  modes.forEach((mode) => mode.addEventListener("click", switchModes));
+
+  document.addEventListener("keyup", (event) => {
+    if (event.target.localName === "input") return;
+    if (event.key === "s") settingsModalPopover.togglePopover();
+  });
+
+  setupActiveToggleEvents(fontButtons);
+  setupActiveToggleEvents(accentColorButtons);
+
+  settingsModalCloseBtn.addEventListener("click", () => settingsModalPopover.hidePopover());
+  settingsModalApplyButton.addEventListener("click", handleSettingsFormSubmit);
+}
+
+function switchModes(event) {
+  const secondsForMode = parseInt(event.target.dataset.time, 10);
+  modes.forEach((mode) => mode.classList.remove("active"));
+  event.target.classList.add("active");
+  getStartedMessage.style.display = "none";
+  timer(secondsForMode);
+}
+
+function setupActiveToggleEvents(buttons) {
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      buttons.forEach((button) => button.classList.remove("active"));
+      event.target.classList.add("active");
+    });
+  });
+}
